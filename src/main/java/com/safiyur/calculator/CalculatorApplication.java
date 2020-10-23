d46b68e68ae90dd6793da12484260a4500f26bb9
safiyur <safiyurrahman@ymail.com>
2020-10-24 00:39:19 +0530

    Update CalculatorApplication.java

--- src/main/java/com/safiyur/calculator/CalculatorApplication.java
+++ src/main/java/com/safiyur/calculator/CalculatorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello jenkins");
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
 
